b21d7d8de753d174b0833f1d2167bb3594b0201a
Miguel Gaviria <74798579+MGE020@users.noreply.github.com>
2022-12-25 12:04:38 -0500

    Actualizar 11340 - Newspaper.cpp

--- uva/11340 - Newspaper.cpp
+++ uva/11340 - Newspaper.cpp
@@ -7,28 +7,28 @@
 
 using namespace std;
 
-//int main() {
-//    int cases, chars, val, lines;
-//    double ttl;
-//    char ch;
-//    scanf("%d", &cases);
-//    while(cases--) {
-//        unordered_map<char, int> map;
-//        ttl = 0;
-//        scanf("%d", &chars);
-//        while(chars--) {
-//            scanf(" %c %d", &ch, &val);
-//            map[ch] = val;
-//        }
-//        scanf("%d\n", &lines);
-//        while(lines--) {
-//            char text[10000];
-//            fgets(text, 10000, stdin);
-//            for(int i = 0; text[i] != 0; ++i) {
-//                ttl += map[text[i]];
-//            }
-//        }
-//        printf("%.2f$\n", ttl/100);
-//    }
-//}
+int main() {
+    int cases, chars, val, lines;
+    double ttl;
+    char ch;
+    scanf("%d", &cases);
+    while(cases--) {
+        unordered_map<char, int> map;
+        ttl = 0;
+        scanf("%d", &chars);
+        while(chars--) {
+            scanf(" %c %d", &ch, &val);
+            map[ch] = val;
+        }
+        scanf("%d\n", &lines);
+        while(lines--) {
+            char text[10000];
+            fgets(text, 10000, stdin);
+            for(int i = 0; text[i] != 0; ++i) {
+                ttl += map[text[i]];
+            }
+        }
+        printf("%.2f$\n", ttl/100);
+    }
+}
 
