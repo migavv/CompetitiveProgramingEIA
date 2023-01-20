a9d308cfc303159664b17265fbc473e15a6d8d8c
Miguel Gaviria <74798579+MGE020@users.noreply.github.com>
2023-01-20 07:31:53 -0500

    Actualizar 11933 - Splitting Numbers.cpp

--- uva/11933 - Splitting Numbers.cpp
+++ uva/11933 - Splitting Numbers.cpp
@@ -4,27 +4,27 @@
 
 #include<cstdio>
 
-//int main() {
-//    int n, a ,b, aux;
-//    bool even;
-//    while(scanf("%d", &n), n) {
-//        a = 0;
-//        b = 0;
-//        even = false;
-//        aux = 1;
-//        while(n) {
-//            if(n & 1) {
-//                if(even) {
-//                    b |= aux;
-//                    even = false;
-//                } else {
-//                    a |= aux;
-//                    even = true;
-//                }
-//            }
-//            aux <<= 1;
-//            n >>= 1;
-//        }
-//        printf("%d %d\n", a, b);
-//    }
-//}
+int main() {
+    int n, a ,b, aux;
+    bool even;
+    while(scanf("%d", &n), n) {
+        a = 0;
+        b = 0;
+        even = false;
+        aux = 1;
+        while(n) {
+            if(n & 1) {
+                if(even) {
+                    b |= aux;
+                    even = false;
+                } else {
+                    a |= aux;
+                    even = true;
+                }
+            }
+            aux <<= 1;
+            n >>= 1;
+        }
+        printf("%d %d\n", a, b);
+    }
+}
